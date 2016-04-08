89b9797085f5bc6bf5b22418732c4e4ef864a85d
Pcandeias <pedro.m94@hotmail.com>
2016-04-08 11:20:15 +0200

    2 new tests to see different fonctionalities

---
+++ src/ch/epfl/xblast/tests/Main.java
@@ -0,0 +1,66 @@
+package ch.epfl.xblast.tests;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import ch.epfl.xblast.Cell;
+import ch.epfl.xblast.PlayerID;
+import ch.epfl.xblast.server.Block;
+import ch.epfl.xblast.server.Board;
+import ch.epfl.xblast.server.GameState;
+import ch.epfl.xblast.server.Player;
+import ch.epfl.xblast.server.debug.GameStatePrinter;
+import ch.epfl.xblast.server.debug.RandomEventGenerator;
+
+public class Main {
+    public static void main(String[] args) {
+        Block __ = Block.FREE;
+        Block XX = Block.INDESTRUCTIBLE_WALL;
+        Block xx = Block.DESTRUCTIBLE_WALL;
+        
+        Board board = Board.ofRows(
+                Arrays.asList(
+                        Arrays.asList(XX, XX, XX, XX, XX, XX, XX, XX, XX, XX, XX, XX, XX, XX, XX),
+                        Arrays.asList(XX, __, __, __, __, __, xx, __, xx, __, __, __, __, __, XX),
+                        Arrays.asList(XX, __, XX, xx, XX, xx, XX, xx, XX, xx, XX, xx, XX, __, XX),
+                        Arrays.asList(XX, __, xx, __, __, __, xx, __, xx, __, __, __, xx, __, XX),
+                        Arrays.asList(XX, xx, XX, __, XX, XX, XX, XX, XX, XX, XX, __, XX, xx, XX),
+                        Arrays.asList(XX, __, xx, __, xx, __, __, __, __, __, xx, __, xx, __, XX),
+                        Arrays.asList(XX, xx, XX, xx, XX, xx, XX, __, XX, xx, XX, xx, XX, xx, XX),
+                        Arrays.asList(XX, __, xx, __, xx, __, __, __, __, __, xx, __, xx, __, XX),
+                        Arrays.asList(XX, xx, XX, __, XX, XX, XX, XX, XX, XX, XX, __, XX, xx, XX),
+                        Arrays.asList(XX, __, xx, __, __, __, xx, __, xx, __, __, __, xx, __, XX),
+                        Arrays.asList(XX, __, XX, xx, XX, xx, XX, xx, XX, xx, XX, xx, XX, __, XX),
+                        Arrays.asList(XX, __, __, __, __, __, xx, __, xx, __, __, __, __, __, XX),
+                        Arrays.asList(XX, XX, XX, XX, XX, XX, XX, XX, XX, XX, XX, XX, XX, XX, XX)));
+        
+        List<Player> players = new ArrayList<>();
+        players.add(new Player(PlayerID.PLAYER_1, 3, new Cell(1,1), 2, 3));
+        players.add(new Player(PlayerID.PLAYER_2, 3, new Cell(13,1), 2, 3));
+        players.add(new Player(PlayerID.PLAYER_3, 3, new Cell(13,11), 2, 3));
+        players.add(new Player(PlayerID.PLAYER_4, 3, new Cell(1,11), 2, 3));
+        
+        GameState gameState = new GameState(board, players);
+        RandomEventGenerator reg = new RandomEventGenerator(2016,30,100);
+        GameStatePrinter.printGameState(gameState);
+        
+        while(!gameState.isGameOver())
+        {
+            try {
+                System.in.read();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            
+            System.out.print("\u001b[2J\u001b[1;1H");
+            gameState = gameState.next(reg.randomSpeedChangeEvents(), reg.randomBombDropEvents());
+            GameStatePrinter.printGameState(gameState);            
+        }
+        
+        
+    }
+
+}
