e72a23a6be323066ee424da0090a5fa4a93fbdc4
Roosembert Palacios <roosemberth.palacios@epfl.ch>
2016-05-09 14:55:08 +0200

    Added static method for getting the player list in roundrobin
    Signed-off-by: Roosembert Palacios <roosemberth.palacios@epfl.ch>

--- src/ch/epfl/xblast/PlayerID.java
+++ src/ch/epfl/xblast/PlayerID.java
@@ -1,5 +1,8 @@
 package ch.epfl.xblast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *  PlayerID Instance associated to each player
  */
@@ -8,4 +11,14 @@ public enum PlayerID {
     PLAYER_2,
     PLAYER_3,
     PLAYER_4;
+    
+    public static List<PlayerID> rotationOrder(PlayerID p){
+        int o = p==null?0:p.ordinal();
+        int size = PlayerID.values().length;
+        List<PlayerID> ret = new ArrayList<>(size);
+        for (int i = 0; i<size; ++i){
+            ret.add(PlayerID.values()[(i+o)%size]);
+        }
+        return ret;
+    }
 }
