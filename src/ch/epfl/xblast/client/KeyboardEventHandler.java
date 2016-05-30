d5ae6fae0e62733cb88a0f7f601371259625ac2c
Roosembert Palacios <roosemberth.palacios@epfl.ch>
2016-05-30 14:13:10 +0200

    Added Authors headers and Copyright on updated tree
    Signed-off-by: Roosembert Palacios <roosemberth.palacios@epfl.ch>

--- src/ch/epfl/xblast/client/KeyboardEventHandler.java
+++ src/ch/epfl/xblast/client/KeyboardEventHandler.java
@@ -1,3 +1,12 @@
+/*
+ * XBlast, CS108 Programming Project
+ *
+ * (C) 2016 - 247128 - Roosembert Palacios <roosembert.palacios@epfl.ch>
+ *          - 246452 - Pedro Miguel Candeias <pedro.candeiasmartins@epfl.ch>
+ * Released under CC BY-NC-SA License: https://creativecommons.org/licenses/
+ */
+
+
 package ch.epfl.xblast.client;
 
 import java.awt.event.KeyAdapter;
